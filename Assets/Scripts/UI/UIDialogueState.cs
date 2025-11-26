5bcb97c2b59047383ae5084ab6ace825b20d48ae
mojtaba.n <mojtaba.n@behinrahkar.com>
2025-11-27 02:09:38 +0330

    feat: add dialogue document file & UI Dialogue State script

---
+++ Assets/Scripts/UI/UIDialogueState.cs
@@ -0,0 +1,30 @@
+using UnityEngine.UIElements;
+
+namespace RPG.UI
+{
+    public class UIDialogueState : UIBaseState
+    {
+        private VisualElement dialogueContainer;
+        private Label dialogueText;
+        private VisualElement nextButton;
+        private VisualElement choicesGroup;
+
+        public UIDialogueState(UIController ui)
+            : base(ui) { }
+
+        public override void EnterState()
+        {
+            dialogueContainer = controller.root.Q("dialogue-container");
+            dialogueText = dialogueContainer.Q<Label>("dialogue-text");
+            nextButton = dialogueContainer.Q<Label>("dialogue-next-button");
+            choicesGroup = dialogueContainer.Q<Label>("choices-group");
+
+            dialogueContainer.style.display = DisplayStyle.Flex;
+        }
+
+        public override void SelectButton()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
