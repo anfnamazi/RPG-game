07b1d764038083edb71c02b5b8e12cda887921c7
mojtaba.n <mojtaba.n@behinrahkar.com>
2025-12-29 00:50:32 +0330

    feat: complete transportation to dungeon scene

--- Assets/Scripts/Core/Portal.cs
+++ Assets/Scripts/Core/Portal.cs
@@ -5,11 +5,20 @@ namespace RPG.Core
 {
     public class Portal : MonoBehaviour
     {
+        private Collider colliderCmp;
+        [SerializeField] private int nextSceneIndex;
+
+        private void Awake()
+        {
+            colliderCmp = GetComponent<Collider>();
+        }
+
         void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag(Constants.PLAYER_TAG)) return;
 
-            print("Player detected!");
+            colliderCmp.enabled = false;
+            SceneTransition.Initiate(nextSceneIndex);
         }
     }
 }
